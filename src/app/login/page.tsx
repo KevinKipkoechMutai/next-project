fcdc9dd271523664a5d4342e608946908663386d
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-08-26 14:05:28 +0300

    base structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="flex m-auto border-2 border-black">
+            <h1 className="text-center text-2xl">Login</h1>
+        </div>
+    )
+}
