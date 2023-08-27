3c2029b2c5e99c08f098528f7574816fae872daa
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-08-27 21:32:19 +0300

    packages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,17 +2,34 @@
 import Link from "next/link"
 import React from "react"
 import { useRouter } from "next/navigation"
-import { axios } from "axios"  
+import axios from "axios" 
+import { toast } from "react-hot-toast" 
 
 export default function LoginPage() {
+    const router = useRouter()
     const [user, setUser] = React.useState({
         email: "",
         password: ""
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
 
     const onLogin = async () => {
-
+        try {
+            
+        } catch (error: any) {
+            console.log("Login failed", error.message)
+            toast.error(error.message)
+        }
     }
+
+    React.useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
