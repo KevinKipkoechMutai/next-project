cb51fa9766cb95a34e84c623ead3922c7d23801c
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-08-28 10:44:20 +0300

    login: done

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -33,7 +33,9 @@ export async function POST(request: NextRequest) {
             username: user.username,
             email: user.email
         }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_ENV!, { expiresIn: "1d" })
+
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
 
         //set token in cookie
         const response = NextResponse.json({
